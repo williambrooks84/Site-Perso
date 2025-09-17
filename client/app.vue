61e944ba2aeb8cf620abf19b0460faf3f37218a3
William Brooks <william.brooks@etu.unilim.fr>
2025-09-17 23:03:45 +0200

    will: réglage changement de thème (à finir)

--- client/app.vue
+++ client/app.vue
@@ -5,3 +5,19 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  try {
+    const theme = localStorage.getItem('theme');
+    if (
+      theme === 'dark' ||
+      (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark');
+    }
+  } catch (e) {}
+});
+</script>
