a6ec5c1ad665751815df3ba873964c417ef4ca07
William Brooks <william.brooks@etu.unilim.fr>
2026-01-21 13:16:39 +0100

    ajout d'un bouton pour télécharger le CV

---
+++ client/components/CV/DownloadCvButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <a
+      :href="href"
+      :download="download"
+      class="btn-primary w-fit"
+    >
+      {{label}}
+    </a>
+</template>
+
+<script setup>
+const props = defineProps({
+    href: {
+        type: String,
+    },
+    download: {
+        type: String,
+    },
+    label: {
+        type: String,
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
--- client/pages/moncv.vue
+++ client/pages/moncv.vue
@@ -1,11 +1,21 @@
 <template>
-    <div class="w-full flex justify-center">
+    <div class="w-full flex flex-col justify-center items-center gap-10">
+        <DownloadCvButton :href="buttonProps[0].href" :download="buttonProps[0].download" :label="buttonProps[0].label" />
         <Cv />
     </div>
 </template>
 
 <script setup>
 import Cv from '~/components/CV/Cv.vue'
+import DownloadCvButton from '~/components/CV/DownloadCvButton.vue'
+
+const buttonProps = [
+    {
+        href: '/assets/pdf/cv.pdf',
+        download: 'William-Brooks-CV.pdf',
+        label: 'Télécharger mon CV'
+    }
+]
 </script>
 
 <style scoped>
