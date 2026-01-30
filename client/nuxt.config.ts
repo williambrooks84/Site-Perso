9a4d13c56acf142d30f999cbbf2be6a65a9d08dc
William Brooks <william.brooks@etu.unilim.fr>
2026-01-30 10:49:11 +0100

    ajou de favicons supplémentaires

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -27,7 +27,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
       ]
     }
   },
